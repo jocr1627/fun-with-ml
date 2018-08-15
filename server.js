0930cdc0f37e42f807c2e3731461ec0252732651
John Cronk <john.cronk@petuum.com>
2018-08-15 13:36:06 -0400

    SYM-1234 Super aweseome feature

--- server.js
+++ server.js
@@ -13,3 +13,7 @@ app.get('/*', function(_, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// My super awesome feature
+
+// another part
